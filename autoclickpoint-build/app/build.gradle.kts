d9943ea4583618bae252eed425c7cde589629a96
MP <194286033+gambler12345@users.noreply.github.com>
2026-09-22 08:47:31 +0200

    chore(autoclick): bump version to 1.0.2

--- autoclickpoint-build/app/build.gradle.kts
+++ autoclickpoint-build/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "de.mpconsulting.autoclickpoint"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
